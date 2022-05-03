65f9cc9f34b99c5e9a26faab31851278a71e2f3c
Samuel ODaniels <samodan195@gmail.com>
2022-05-03 19:15:19 +0100

    Optimize program to stop loop if arr is already sorted

--- main.rb
+++ main.rb
@@ -1,11 +1,16 @@
 def bubble_sort(arr)
   (arr.length - 1).times do
+    swapped = false
     for i in 0...arr.length - 1
       if arr[i] > arr[i + 1] 
         temp = arr[i] #swap their positions
         arr[i] = arr[i + 1]
         arr[i + 1] = temp
+        swapped = true 
       end 
+    end
+    if !swapped 
+    break #stops the loop if array is already sorted
     end
   end
   arr
