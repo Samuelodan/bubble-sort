a85ef984490ec91d0c4bb17aa5dc07def24a1046
Samuel ODaniels <samodan195@gmail.com>
2022-06-11 20:38:08 +0100

    Swap positions without using an extra variable

--- main.rb
+++ main.rb
@@ -1,14 +1,12 @@
 def bubble_sort(arr)
   len = arr.length
-  (arr.length - 1).times do
+  (len - 1).times do
     swapped = false
     for i in 0...len - 1
-      if arr[i] > arr[i + 1] 
-        temp = arr[i] #swap their positions
-        arr[i] = arr[i + 1]
-        arr[i + 1] = temp
-        swapped = true 
-      end 
+      if arr[i] > arr[i + 1]
+        arr[i], arr[i + 1] = arr[i + 1], arr[i] # swap their positions
+        swapped = true
+      end
     end
     len -= 1
     if !swapped 
